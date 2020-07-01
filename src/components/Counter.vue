4c04f5fc85a8dba86abacae31e55db2e6a70fdc6
drsg <yaoxs1949@foxmail.com>
2020-07-01 18:25:35 +0800

    Vuex初试

---
+++ src/components/Counter.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>{{ count }}</h1>
+    <h2>{{ this.$store.state.count }}</h2>
+    <h3>{{ this.$store.getters.getTodoById(1) }}</h3>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  computed: {
+    ...mapState(['count']),
+  },
+};
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,26 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    count: 3,
+    theme: 'light',
+    expand: true,
+    todos: [
+      { id: 1, text: 'listen music', done: true },
+      { id: 2, text: 'play games', done: false },
+    ],
+  },
+  mutations: {
+    increment(state) {
+      state.count++;
+    },
+    triggerTheme(state) {
+      state.theme = state.theme === 'light' ? 'dark' : 'light';
+    },
+  },
+  getters: {
+    getTodoById: state => id => state.todos.find(todo => todo.id === id),
+  },
   actions: {},
   modules: {},
 });
